ac0c7a04ab264ea8f028e435ce5e7684b92f540f
Junghee Yang <junghee.yang@mobiliya.com>
2021-01-06 05:14:27 +0900

    update resource creation scenario2

---
+++ create-res-scenario-2.sh
@@ -0,0 +1,95 @@
+
+# !/bin/bash
+# Purpose: create resources as refer to a csv file.
+# Author : Junghee Yang(junghee.yang@quest-global.com)
+# Date : 5-Jan-2021
+# -------------------------------------------------------
+source check-status.sh
+exec_shell="sh optstr-from-csv.sh"
+
+# Create Resource group
+echo "--------------- Start [Create Resource Group] -----------------"
+output=$($exec_shell "csvFiles/rg.csv" "az group create")
+status=$(check_status)
+if [ status == "fail" ]; then
+    echo "Fail to create resource group"
+    exit 1
+else
+    echo $output
+fi
+echo "----------------- End [Create Resource Group] -----------------"
+
+echo "-------------------- Start [Create VNet]  ---------------------"
+# Create VNet resource
+output=$($exec_shell "csvFiles/vnet.csv" "az network vnet create") 
+echo $output
+echo "-------------------- End [Create VNet]  -----------------------"
+
+echo "--------------- Start [Create Availability Set] ---------------"
+# Create Avaliability Set
+output=$($exec_shell "csvFiles/avail.csv" "az vm availability-set create")
+echo $output
+echo "---------------- End [Create Availability Set] ----------------"
+
+echo "-------------------- Start [Create NSG] -----------------------"
+# Create NSG resource
+output=$($exec_shell "csvFiles/nsg.csv" "az network nsg create")
+echo $output
+echo "-------------------- End [Create NSG] -------------------------"
+
+echo "-------------------- Start [Create Subnet] --------------------"
+# Create Subnet resource
+output=$($exec_shell "csvFiles/subnet.csv" "az network vnet subnet create")
+echo $output
+echo "---------------------- End [Create Subnet] --------------------"
+
+echo "--------------- Start [Create Public IP] ----------------------"
+#Create PIP resource
+output=$($exec_shell "csvFiles/pip.csv" "az network public-ip create")
+echo $output
+echo "----------------- End [Create Public IP] ----------------------"
+
+echo "--------------- Start [Create Load Balancer] ------------------"
+#Create Load Balancer
+output=$($exec_shell "csvFiles/lb.csv" "az network lb create")
+echo $output
+echo "----------------- End [Create Load Balancer] ------------------"
+
+echo "----------------- Start [Create Probe] ------------------------"
+#Create Probe of Load Balancer
+output=$($exec_shell "csvFiles/probe.csv" "az network lb probe create")
+echo $output
+echo "----------------- End [Create Probe] --------------------------"
+
+echo "--------------- Start [Create LB rule] ------------------------"
+#Create Rule of Load Balancer
+output=$($exec_shell "csvFiles/lbrule.csv" "az network lb rule create")
+echo $output
+echo "----------------- End [Create LB rule] ------------------------"
+
+echo "------------------ Start [Create NIC] -------------------------"
+#Create Network Interface
+output=$($exec_shell "csvFiles/nic.csv" "az network nic create")
+echo $output
+echo "-------------------- End [Create NIC] -------------------------"
+
+echo "------------ Start [Create NIC IP-config] ---------------------"
+#Create NIC IP config
+output=$($exec_shell "csvFiles/nic-ipconfig.csv" "az network nic ip-config create")
+echo "------------- End [Create NIC IP-config] ----------------------"
+
+echo "------------------- Start [Create VM] -------------------------"
+#Create VM
+output=$($exec_shell "csvFiles/vm-3.csv" "az vm create")
+echo $output
+echo "-------------------- End [Create VM] --------------------------"
+
+
+# Create NSG rule
+#output=$($exec_shell "csvFiles/nsgrule.csv" "az network nsg rule create")
+#echo $output
+
+# Create VM resource
+#output=$($exec_shell "csvFiles/vm-2.csv" "az vm create")
+#echo $output
+
---
+++ delete-res-scenario-2.sh
@@ -0,0 +1,46 @@
+# !/bin/bash
+# Purpose: delete resources as refer to a csv file.
+# Author : Junghee Yang(junghee.yang@quest-global.com)
+# Date : 6-Jan-2020
+# -------------------------------------------------------
+exec_shell="sh optstr-ext-from-csv.sh"
+
+echo "---------------------- Start [Delete VM] -----------------------------"
+# Delete VM resource
+echo $($exec_shell "csvFiles/vm-3.csv" "az vm delete -y" "name resource-group")
+echo "----------------------- End [Delete VM] ------------------------------"
+
+echo "---------------------- Start [Delete IP Config]-----------------------"
+echo $($exec_shell "csvFiles/nic-ipconfig.csv" "az network nic ip-config delete" "name resource-group nic-name")
+echo "------------------------ End [Delete IP Config]-----------------------"
+
+echo "---------------------- Start [Delete NIC] ----------------------------"
+echo $($exec_shell "csvFiles/nic.csv" "az network nic delete" "name resource-group")
+echo "------------------------ End [Delete NIC] ----------------------------"
+
+echo "---------------------- Start [Delete LB] -----------------------------"
+echo $($exec_shell "csvFiles/lb.csv" "az network lb delete" "name resource-group")
+echo "------------------------ End [Delete LB] -----------------------------"
+
+echo "------------------- Start [Delete Public IP] -------------------------"
+echo $($exec_shell "csvFiles/pip.csv" "az network public-ip delete" "name resource-group")
+echo "-------------------- End [Delete Public IP] --------------------------"
+
+echo "--------------------- Start [Delete Subnet] --------------------------"
+echo $($exec_shell "csvFiles/subnet.csv" "az network vnet subnet delete" "name resource-group vnet-name"
+echo "---------------------- End [Delete Subnet] ---------------------------"
+
+echo "----------------------- Start [Delete NSG] ---------------------------"
+echo $($exec_shell "csvFiles/nsg.csv" "az network nsg delete" "name resource-group")
+echo "------------------------ End [Delete NSG] ----------------------------"
+
+echo "----------------- Start [Delete Availability Set] --------------------"
+echo $($exec_shell "csvFiles/avail.csv" "az vm availability-set delete" "name resource-group")
+echo "------------------ End [Delete Availability Set] ---------------------"
+
+echo "----------------------- Start [Delete VNet] --------------------------"
+echo $($exec_shell "csvFiles/vnet.csv" "az network vnet delete" "name resource-group")
+echo "------------------------ End [Delete VNet] ---------------------------"
+
+#sh delete-unused-res.sh
+
